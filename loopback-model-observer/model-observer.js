7faab223ca91b74847db08b15a37d03d90df9e86
RSR <srinivasaraghavan_r@infosys.com>
2017-08-04 16:37:58 +0530

    add require of utils

--- loopback-model-observer/model-observer.js
+++ loopback-model-observer/model-observer.js
@@ -6,6 +6,7 @@
  */
 var loopback = require('loopback');
 var _ = require('lodash');
+var utils = require('../common/utils');
 
 module.exports = function(RED) {
 
@@ -22,14 +23,19 @@ module.exports = function(RED) {
         if (Model !== undefined) {
 
             Model.on(event, function (eventPayload) {
-                var ctx = null;                // If node was created and context was stored then check for payload for callContext                if (node.callContext && node.callContext.ctx && eventPayload) {
-                    ctx = eventPayload.ctx || eventPayload.callContext || eventPayload.options;                    if (!ctx) {
-                        console.log("NODE-RED Error : payload didn't have call context in ctx, callContext or options properties of payload.");                        return;
+                var ctx = null;
+                // If node was created and context was stored then check for payload for callContext
+                if (node.callContext && node.callContext.ctx && eventPayload) {
+                    ctx = eventPayload.ctx || eventPayload.callContext || eventPayload.options;
+                    if (!ctx) {
+                        console.log("NODE-RED Error : payload didn't have call context in ctx, callContext or options properties of payload.");
+                        return;
                     }
                     if (ctx && ctx.options) {
                         ctx = ctx.options;
                     }
-                }
+                }
+
                 if (!utils.compareContext(node, { Model: Model, options: { ctx: ctx } } )) {
                     return next();
                 }
