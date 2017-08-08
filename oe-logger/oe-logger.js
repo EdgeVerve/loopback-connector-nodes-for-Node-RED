a240f6b947d541c17968cf4cbcb1016ae1e2ebfe
Karin Angel <karin.angel@edgeverve.com>
2017-08-08 21:27:47 +0300

    added a possibility to add payload and instance properties to logs

--- oe-logger/oe-logger.js
+++ oe-logger/oe-logger.js
@@ -12,16 +12,33 @@ module.exports = function(RED) {
         var node = this;
         var context = node.callContext.ctx;
         var flowName = config.flowName;
-        var message = config.message;
+        if (flowName === '') {
+            flowName = 'default-flow';
+        }
         var levelOfLog = config.levelOfLog;
+        if (levelOfLog === '') {
+            levelOfLog = 'info';
+        }
+        var message = {};
+        if (config.message !== '') {
+            message.message = config.message;
+        }
         var log = oeLogger(flowName);
+        var addPayload = config.addPayload;
+        var addInstance = config.addInstance;
         node.on('input', function(msg) {
+            if (msg && msg.payload && addPayload === '1') {
+               message.payload = msg.payload; 
+            }
+            if (msg && msg.ctx && msg.ctx.instance && addInstance === '1') {
+                message.instance = msg.ctx.instance;
+            }
             if (msg && msg.ctx && msg.ctx.options) {
                 context = msg.ctx.options;
             } else if (msg && msg.callContext) {
                 context = msg.callContext;
             }
-            log[levelOfLog](context, message);
+            log[levelOfLog](context, JSON.stringify(message));
             node.send(msg);
         });
     }
